386b50d9c6cf08fc217e3766958e336b75ae5d89
nodakbarnes <wally@nodakengineering.com>
2022-05-05 17:26:31 -0500

    Refactored switch in playRound function

--- rps.js
+++ rps.js
@@ -1,72 +1,89 @@
-function computerPlay() {
-	// Randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
-	const choices = ['rock', 'paper', 'scissors'];
-	const randomNumber = Math.floor(Math.random() * choices.length);
-	return choices[randomNumber];
+function randomChoice() {
+    // Randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomNumber];
 }
 
 function playRound(playerSelection, computerSelection) {
+    let result = 'default';
+    
+    switch (playerSelection) {
+        case 'rock':
+            if (computerSelection === 'rock') {
+                result = 'draw';
+            } else if (computerSelection === 'paper') {
+                result = 'lose';
+            } else {
+                result = 'win';
+            }
+            break;
+        case 'paper':
+            if (computerSelection === 'paper') {
+                result = 'draw';
+            } else if (computerSelection === 'scissors') {
+                result = 'lose';
+            } else {
+                result = 'win';
+            }
+            break;
+        case 'scissors':
+            if (computerSelection === 'scissors') {
+                result = 'draw';
+            } else if (computerSelection === 'rock') {
+                result = 'lose';
+            } else {
+                result = 'win';
+            }
+            break;
+        default:
+            console.log('Please choose rock, paper or scissors');
+    }
 
-	if (playerSelection === computerSelection) return 'tie';
-	
-	switch (playerSelection) {
-		case 'rock':
-			if (computerSelection === 'paper') return 'lose';
-			return 'win';
-			break;
-		case 'paper':
-			if (computerSelection === 'scissors') return 'lose';
-			return 'win';
-			break;
-		case 'scissors':
-			if (computerSelection === 'rock') return 'lose';
-			return 'win';
-			break;
-		default:
-			console.log('Please choose rock, paper or scissors');
-	}
+    return result;
 }
 
 function game() {
-	let playerScore = 0;
-	let computerScore = 0;
-	
-	for (let i = 0; i < 5; i++) {
-		let playerSelection = prompt(`Round ${i + 1}: Please choose rock, paper or scissors`);
-		playerSelection = playerSelection.toLowerCase();
-		const computerSelection = computerPlay();
-		const tieString = `It's a tie. Both chose ${playerSelection}.`;
-		const loseString = `You lose! ${computerSelection} beats ${playerSelection}.`;
-		const winString = `You win! ${playerSelection} beats ${computerSelection}.`;
-		const roundResult = playRound(playerSelection, computerSelection);
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        // let playerSelection = prompt(`Round ${i + 1}: Please choose rock, paper or scissors`, randomChoice());
+        // playerSelection = playerSelection.toLowerCase();
+        const playerSelection = randomChoice();
+        const computerSelection = randomChoice();
+        const drawString = `It's a draw. Both chose ${playerSelection}.`;
+        const loseString = `You lose! ${computerSelection} beats ${playerSelection}.`;
+        const winString = `You win! ${playerSelection} beats ${computerSelection}.`;
+        const roundResult = playRound(playerSelection, computerSelection);
 
-		switch (roundResult) {
-			case 'win':
-				console.log(winString);
-				playerScore += 1;
-				break;
-			case 'lose':
-				console.log(loseString);
-				computerScore += 1;
-				break;
-			case 'tie':
-				console.log(tieString);
-				break;
-			default:
-				console.log('That round did not count');
-				i -= 1;
-		}
+        switch (roundResult) {
+            case 'win':
+                console.log(winString);
+                playerScore += 1;
+                break;
+            case 'lose':
+                console.log(loseString);
+                computerScore += 1;
+                break;
+            case 'draw':
+                console.log(drawString);
+                break;
+            default:
+                console.log('That round did not count');
+                i -= 1;
+        }
 
-	}
-	
-	console.log(`Final score - You: ${playerScore} Computer: ${computerScore}`);
-	if (playerScore > computerScore) {
-		console.log("You won the game!");
-	} else if (computerScore > playerScore) {
-		console.log("Computer won the game!");
-	} else {
-		console.log("Tie game!");
-	}
+    }
+    
+    console.log(`Final score - You: ${playerScore}, Computer: ${computerScore}`);
+    if (playerScore > computerScore) {
+        console.log("You won the game!");
+    } else if (computerScore > playerScore) {
+        console.log("Computer won the game!");
+    } else {
+        console.log("Tie game!");
+    }
 }
 
 game();
